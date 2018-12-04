40f5f5b102a16631e414944a1b0b0394f0f3a96d
modi160497 <30540253+modi160497@users.noreply.github.com>
2018-12-03 23:59:53 -0500

    Update login.py

--- login.py
+++ login.py
@@ -4,6 +4,19 @@ import getpass
 import clioptions
 print("Welcome to Cloud9 proceed to Login")
 
+mydb = mysql.connector.connect(
+  host="cloudnine.c87lmy1ftwtu.us-east-2.rds.amazonaws.com",
+  user="modi1234",
+  passwd="Was160497",
+  database="CloudNine"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT username FROM user")
+
+myresult = mycursor.fetchall()
+
 with open("userlogin.txt") as f:
     content = f.read().splitlines()
     array=content[0].split()
@@ -13,22 +26,25 @@ CorrectUsername = array[0]
 CorrectPassword = array[1]
 
 def userlogin():
-
     boolvar = 'true'
     while (boolvar == 'true'):
         user = input("Please enter your username:  ")
-        if user == CorrectUsername:
-            while True:
-                pwd = getpass.getpass(prompt='enter your password:  ')
-
-                if pwd == CorrectPassword:
-                    print("Welcome!!!")
-                    boolvar = 'false'
-                    clioptions.options()
-                    break
-                else:
-                    print
-                    "The password you entered is incorrect."
+        for y in myresult:
+		    x = ''.join(y)
+		    if(x==user):
+                while True:
+                        pwd = getpass.getpass(prompt='enter your password:  ')
+                        mycursor.execute("SELECT passwd FROM user")
+  			            myresult = mycursor.fetchall()
+            		    for y in myresult:
+				            x = ''.join(y)
+  				            if(x==pwd):
+                                print("Welcome!!!")
+                                boolvar = 'false'
+                                clioptions.options()
+                                break
+                            else:
+                                print("The password you entered is incorrect.")
             else:
                 print("incorrect username")
     return
